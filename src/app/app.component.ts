ec234f8480a140a001a271966005ee6496797f25
Pushkin Anton <pushkinanton2002@gmail.com>
2025-04-30 19:41:24 +0300

    create ThreeComponent commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { ToDoItemsComponent } from './to-do-items/to-do-items.component';
+import { TwoComponentComponent } from './two-component/two-component.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, ToDoItemsComponent],
+  imports: [
+    RouterOutlet, 
+    ToDoItemsComponent, 
+    TwoComponentComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
---
+++ src/app/three-component/three-component.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-three-component',
+  imports: [],
+  templateUrl: './three-component.component.html',
+  styleUrl: './three-component.component.css'
+})
+export class ThreeComponentComponent {
+
+}
