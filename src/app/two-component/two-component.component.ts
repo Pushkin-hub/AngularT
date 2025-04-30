51cf2c3d9344b9cfdccffbddcfe95ee595288dfc
Pushkin Anton <pushkinanton2002@gmail.com>
2025-04-30 19:06:14 +0300

    create item commit

---
+++ src/app/two-component/two-component.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-two-component',
+  imports: [],
+  templateUrl: './two-component.component.html',
+  styleUrl: './two-component.component.css'
+})
+export class TwoComponentComponent {
+
+}
